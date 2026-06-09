0984852e2d81205bf15290fadd8a8143ecbafc2a
Fraut <faturrohim1211@gmail.com>
2026-06-09 13:43:22 +0700

    chore: migrate and update hotel database schema with additional room records

--- database/hotel_db.sql
+++ database/hotel_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 02 Jun 2026 pada 13.58
+-- Waktu pembuatan: 09 Jun 2026 pada 08.42
 -- Versi server: 10.4.32-MariaDB
 -- Versi PHP: 8.2.12
 
@@ -42,10 +42,19 @@ CREATE TABLE `kamar` (
 --
 
 INSERT INTO `kamar` (`id_kamar`, `nomor_kamar`, `tipe_kamar`, `harga`, `status`, `foto`, `created_at`) VALUES
-(5, '101', 'Standard', 500000, 'tersedia', '', '2026-06-02 04:11:03'),
-(7, '102', 'Standard', 500000, 'tersedia', '', '2026-06-02 05:54:01'),
-(8, '103', 'Standard', 500000, 'tersedia', '', '2026-06-02 05:54:15'),
-(9, '104', 'Deluxe', 800000, 'tersedia', '', '2026-06-02 05:54:59');
+(5, '101', 'Standard', 500000, 'tersedia', 'room_1780985836_1349.png', '2026-06-02 04:11:03'),
+(7, '102', 'Standard', 500000, 'tersedia', 'room_1780985828_4485.png', '2026-06-02 05:54:01'),
+(8, '103', 'Standard', 500000, 'tersedia', 'room_1780985819_1949.png', '2026-06-02 05:54:15'),
+(9, '201', 'Deluxe', 800000, 'tersedia', 'room_1780985806_5646.png', '2026-06-02 05:54:59'),
+(10, '202', 'Deluxe', 800000, 'tersedia', 'room_1780985792_6469.png', '2026-06-02 16:22:31'),
+(11, '301', 'Suite', 1200000, 'tersedia', 'room_1780985778_8608.png', '2026-06-02 16:22:47'),
+(12, '302', 'Suite', 1200000, 'tersedia', 'room_1780985771_3004.png', '2026-06-02 16:23:00'),
+(13, '401', 'Presidential', 1500000, 'tersedia', 'room_1780985762_8677.png', '2026-06-02 16:23:26'),
+(14, '402', 'Presidential', 1500000, 'tersedia', 'room_1780985754_2465.png', '2026-06-02 16:23:45'),
+(15, '501', 'Family', 1800000, 'tersedia', 'room_1780985732_3093.png', '2026-06-02 16:26:19'),
+(16, '502', 'Family', 1800000, 'tersedia', 'room_1780985723_9272.png', '2026-06-02 16:26:29'),
+(17, '601', 'Exclusive', 2200000, 'tersedia', 'room_1780985715_1136.png', '2026-06-02 16:26:45'),
+(18, '602', 'Exclusive', 2200000, 'tersedia', 'room_1780985706_4717.png', '2026-06-02 16:26:58');
 
 -- --------------------------------------------------------
 
@@ -70,7 +79,8 @@ CREATE TABLE `reservasi` (
 
 INSERT INTO `reservasi` (`id_reservasi`, `id_user`, `id_kamar`, `check_in`, `check_out`, `total_harga`, `status`, `created_at`) VALUES
 (2, 2, 5, '2026-06-02', '2026-06-05', 1500000, 'checkout', '2026-06-02 04:13:49'),
-(3, 2, 9, '2026-06-02', '2026-06-06', 3200000, 'checkout', '2026-06-02 05:56:31');
+(3, 2, 9, '2026-06-02', '2026-06-06', 3200000, 'checkout', '2026-06-02 05:56:31'),
+(4, 2, 7, '2026-06-02', '2026-06-03', 500000, 'checkout', '2026-06-02 15:45:03');
 
 -- --------------------------------------------------------
 
@@ -129,13 +139,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT untuk tabel `kamar`
 --
 ALTER TABLE `kamar`
-  MODIFY `id_kamar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id_kamar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT untuk tabel `reservasi`
 --
 ALTER TABLE `reservasi`
-  MODIFY `id_reservasi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_reservasi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT untuk tabel `users`
